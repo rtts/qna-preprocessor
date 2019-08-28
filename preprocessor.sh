acf0f9b7d04a4605cd789bf6366a0cae1dd51ef8
Jaap Joris Vens <jj@rtts.eu>
2019-08-28 15:57:30 +0200

    disable inotify for testing

--- preprocessor.sh
+++ preprocessor.sh
@@ -7,12 +7,12 @@ FRAMERATE=1
 IMAGE=image.jpg
 touch $IMAGE
 
-(
-    inotifywait -q -m -e close_write $IMAGE | while read event
-    do
-        >&2 echo -n "."
-    done
-) &
+#(
+#    inotifywait -q -m -e close_write $IMAGE | while read event
+#    do
+#        >&2 echo -n "."
+#    done
+#) &
 
 ffmpeg -y -hide_banner -loglevel panic \
        -re -i - \
